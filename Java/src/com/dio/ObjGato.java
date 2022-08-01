f09fe331841df3fa91171fecf3ffa360f102f510
Gabriel Áquila <89168615+GabrielAquila@users.noreply.github.com>
2022-07-31 21:12:25 -0300

    Add files via upload

---
+++ Java/src/com/dio/ObjGato.java
@@ -0,0 +1,11 @@
+package com.dio;
+
+import br.com.dio.model.Gato;
+
+public class ObjGato {
+	public static void main(String[] args) {
+		Gato gato = new Gato("Lua", "Branco/Preto", 1);
+		System.out.println(gato);
+		
+	}
+}
---
+++ Java/src/com/dio/ProgramaSoma.java
@@ -0,0 +1,11 @@
+package com.dio;
+
+public class ProgramaSoma {
+	public static void main(String[] args) {
+		int number1 = 1;
+		int number2 = 2;
+		int soma = number1 + number2;
+		System.out.println("a soma do number1 + number2 é: "+ soma);
+	}
+
+}
